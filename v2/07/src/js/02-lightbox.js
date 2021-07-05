aabb5f36acd76e674cfdd2c6269c774baafd67e7
Alexander Repeta <alexander.repeta@gmail.com>
2021-07-05 18:52:46 +0300

    add files for multiple tasks

---
+++ v2/07/src/js/02-lightbox.js
@@ -0,0 +1,4 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
--- v2/07/src/js/gallery-items.js
+++ v2/07/src/js/gallery-items.js
@@ -1,4 +1,4 @@
-const galleryItems = [
+export const galleryItems = [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
