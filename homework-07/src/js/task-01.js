5e2a3f542cbf962e97f6ecc4b44bb59d0dcf7006
AleksDu <aleksandr.dyshkin@gmail.com>
2021-10-22 16:16:43 +0300

    Update task-01.js

--- homework-07/src/js/task-01.js
+++ homework-07/src/js/task-01.js
@@ -1,8 +1,30 @@
-const titleElements = document.querySelectorAll('h2')
+// const titleElements = document.querySelectorAll('h2')
 
-console.log(`у списку ${titleElements.length} категорії`);
+// console.log(`у списку ${titleElements.length} категорії`);
 
-titleElements.forEach((all) => {
-    console.log("Категорія:", all.textContent);
-    console.log("Кількість елементів:", all.nextElementSibling.children.length);
-});
+// titleElements.forEach((all) => {
+//     console.log("Категорія:", all.textContent);
+//     console.log("Кількість елементів:", all.nextElementSibling.children.length);
+// });
+
+const first = [1, 3, 3, 4, 6, 5, 4];
+const second = [6, 3, 5, 2, 2];
+// const third = [...first, ...second];
+// let third = first
+//   .concat(second)
+//   .third.sort(function (a, b) {
+//     return a > b;
+//   })
+//   .filter(function (num, index) {
+//     return num !== third[index + 1];
+//   });
+
+// const uniqieNumbers = third.filter(
+//   (item, index) => third.indexOf(item) !== index
+// // );
+// const uniqieNumbers = third.reduce(
+//   (unique, item) => (unique.includes(item) ? unique : [...unique, item]),
+//   []
+// );
+
+console.log(third);
