a3e07e15f77ee5052d1ebcfe5f47da2b1ac59c2d
AleksDu <aleksandr.dyshkin@gmail.com>
2021-10-24 11:21:30 +0300

    add totdo

--- homework-07/src/js/task-01.js
+++ homework-07/src/js/task-01.js
@@ -7,24 +7,202 @@
 //     console.log("Кількість елементів:", all.nextElementSibling.children.length);
 // });
 
-const first = [1, 3, 3, 4, 6, 5, 4];
-const second = [6, 3, 5, 2, 2];
-function uniqieNumbers(first, second) {
-  let arr = [];
+/**
+ * Implement a logic of actions and invoke those actions below:
+ */
 
-  if (first !== second) {
-    return arr.push(first);
+// Invoke "Submit for Approval" for softwareEngineerVacancy
+// Invoke "Approve" for softwareEngineerVacancy
+
+// Invoke "Submit for Approval" for frontEndEngineerVacancy
+// Invoke "Reject" for frontEndEngineerVacancy
+
+// /
+//  * Implement the service below the comment block
+// const recordType = 'Vacancy';
+// const { SoftwareEngineer0001, FrontEndEngineer0020 } = 'nameVacancy';
+
+// class Service {
+//   constructor(nameVacancy) {
+//     this.nameVacancy = nameVacancy;
+//   }
+//   SubmitForApproval() {
+//     console.log(`Submitted for approval Vacancy: ${this.nameVacancy}`);
+//   }
+//   Approve() {
+//     console.log(`Approved Record: ${this.nameVacancy}`);
+//   }
+//   Reject() {
+//     console.log(`Reject Record: ${this.nameVacancy}`);
+//   }
+// }
+
+// class Record extends Service {
+//   constructor(config) {
+//     super(config);
+//     console.log(config);
+//   }
+//   id;
+//   type;
+
+//   Record(id, type) {
+//     this.id = id;
+//     this.type = type;
+//   }
+
+//   getId() {
+//     return this.id;
+//   }
+
+//   getType() {
+//     return this.type;
+//   }
+// }
+
+// const softwareEngineerVacancy = new Record('SoftwareEngineer0001', recordType);
+// const frontEndEngineerVacancy = new Record('FrontEndEngineer0020', recordType);
+
+// softwareEngineerVacancy.SubmitForApproval();
+// softwareEngineerVacancy.Approve();
+// frontEndEngineerVacancy.SubmitForApproval();
+// frontEndEngineerVacancy.Reject();
+// // console.log(getType());
+// // console.log(Record.id);
+// console.log(softwareEngineerVacancy);
+// console.log(new Record('SoftwareEngineer0001', recordType));
+
+/**
+ * Implement a logic of actions and invoke those actions below:
+ */
+
+// Invoke "Submit for Approval" for softwareEngineerVacancy
+// Invoke "Approve" for softwareEngineerVacancy
+
+// Invoke "Submit for Approval" for frontEndEngineerVacancy
+// Invoke "Reject" for frontEndEngineerVacancy
+
+/**
+ * Implement the service below the comment block
+
+
+
+*/
+
+class Service {
+  constructor(id, type) {
+    this.id = id;
+    this.type = type;
+  }
+  SubmitForApproval() {
+    console.log(`Submitted for approval Vacancy: ${this.id}`);
+  }
+
+  Approve() {
+    console.log(`Approved Record: ${this.id}`);
+  }
+
+  Reject() {
+    console.log(`Reject Record: ${this.id}`);
   }
 }
-console.log(arr);
 
-const third = [...first, ...second].filter(
-  (number, index, array) => array.indexOf(number) !== index
-);
+class Record extends Service {
+  constructor(config, config2) {
+    super(config, config2);
+    this.Record(config, config2);
+  }
+  id;
+  type;
 
-var array3 = first.filter(function (obj) {
-  return second.indexOf(obj) !== 0;
-});
+  Record(id, type) {
+    this.id = id;
+    this.type = type;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getType() {
+    return this.type;
+  }
+}
+
+const recordType = 'Vacancy';
+
+const softwareEngineerVacancy = new Record('SoftwareEngineer0001', recordType);
+const frontEndEngineerVacancy = new Record('FrontEndEngineer0020', recordType);
+softwareEngineerVacancy.SubmitForApproval();
+softwareEngineerVacancy.Approve();
+
+frontEndEngineerVacancy.SubmitForApproval();
+frontEndEngineerVacancy.Reject();
+
+// Submitted for approval Vacancy: SoftwareEngineer0001
+// Approved Record: SoftwareEngineer0001
+// Submitted for approval Vacancy: FrontEndEngineer0020
+// Rejected Record: FrontEndEngineer0020
+// const first = [1, 3, 3, 4, 6, 5, 4];
+// const second = [6, 3, 5, 2, 2];
+// let uniq = [
+//   ...first.filter(i => !second.includes(i)),
+//   ...second.filter(i => !first.includes(i)),
+// ];
+// let uniqN = [...new Set(uniq)];
+// console.log(uniqN);
+// let intersection = first.filter(num => !second.includes(num));
+// console.log(intersection);
+
+// let Uniq = first
+//   .filter(a => !second.includes(a))
+//   .concat(second.filter(a => !first.includes(a)));
+// let union = Uniq.filter[...new Set([...first, ...second])];
+// console.log(union);
+
+// function complement(a1, a2) {
+//   return a1.filter(v => a2.indeaOf(v) === -1);
+// }
+// console.log(complement(first, second));
+
+// var diff = function (first, second) {
+//   var simmetricDifference = [];
+
+//   var getMissedItems = function (sourceArray, searchInArray) {
+//     sourceArray.forEach(function (item) {
+//       if (
+//         searchInArray.indexOf(item) === -1 &&
+//         simmetricDifference.indexOf(item) === -1
+//       ) {
+//         simmetricDifference.push(item);
+//       }
+//     });
+//   };
+
+//   getMissedItems(first, second);
+//   getMissedItems(second, first);
+
+//   return simmetricDifference;
+// };
+// console.log(diff);
+
+// second.filter(a => first.indexOf(a) == -1);
+// console.log(first);
+// function uniqieNumbers(first, second) {
+//   let arr = [];
+
+//   if (first !== second) {
+//     return arr.push(first);
+//   }
+// }
+// console.log(arr);
+
+// const third = [...first, ...second].filter(
+//   (number, index, array) => array.indexOf(number) !== index
+// );
+
+// var array3 = first.filter(function (obj) {
+//   return second.indexOf(obj) !== 0;
+// });
 // let third = first
 //   .concat(second)
 //   .third.sort(function (a, b) {
@@ -53,4 +231,4 @@ var array3 = first.filter(function (obj) {
 
 //   return commonElements;
 // };
-console.log(array3);
+// console.log(array3);
---
+++ homework-07/src/js/todo.js
@@ -0,0 +1,38 @@
+let currentId = 0;
+
+const getTask = text => `
+    <li id="task-${++currentId}"">
+    <span>${text}</span>
+    <button type="button" class="done" aria-label="Done">
+        &radic;
+    </button>
+    <button type="button" class="close" aria-label="Close">
+        &times;
+    </button>
+    </li>
+`;
+
+const listRef = document.querySelector('.todo');
+const inputRef = document.querySelector('.content');
+
+const createTask = text => {
+  listRef.insertAdjacentHTML('beforeend', getTask(text));
+
+  const task = listRef.querySelector(`#task-${currentId}`);
+  task.querySelector('.close').addEventListener('click', e => {
+    task.remove();
+  });
+
+  const done = task.querySelector('.done');
+  const doneEvent = () => {
+    task.style.background = 'green';
+    done.removeEventListener('click', doneEvent);
+  };
+  done.addEventListener('click', doneEvent);
+};
+
+document.getElementById('todo-button-add').addEventListener('click', () => {
+  const text = inputRef.value;
+  inputRef.value = '';
+  createTask(text);
+});
